1fe4cf8889625cbef6a30cfbb23de34ce09a76c3
Molodoy03 <itelniy@mail.ua>
2021-10-14 12:15:31 +0300

    fix hover for cards in our_leadership module

--- wp-content/themes/bluealliance/modules/our_leadership/our_leadership.php
+++ wp-content/themes/bluealliance/modules/our_leadership/our_leadership.php
@@ -5,7 +5,7 @@
 <?php if ($title || $text || !empty($leadership)) {
     wp_enqueue_style('our_leadership_styles', get_template_directory_uri() . '/static/css/modules/our_leadership/our_leadership.css', '', '', 'all'); ?>
 
-    <section class="our-leadership">
+    <section class="our-leadership position-relative overflow-hidden">
         <div class="container">
             <div class="section-holder">
                 <div class="top-holder">
@@ -22,27 +22,42 @@
                     <?php } ?>
                 </div>
 
-                <?php if (!empty($leadership)) { ?>
-                    <div class="leadership-holder wow fadeInUp">
-                        <?php foreach ($leadership as $id) { ?>
-                            <a class="card" href="<?php echo get_the_permalink($id); ?>">
+                <?php
+                if (!empty($leadership)):
+                    $delay = 0;
+                    ?>
+                    <div class="leadership-holder position-relative overflow-hidden">
+                        <?php foreach ($leadership as $id): ?>
+                            <a class="card wow fadeInUp <?php echo get_field('text_single_page', $id) ? 'hoverable' : '' ?>"
+                               href="<?php the_permalink($id); ?>" data-wow-delay="<?php echo $delay; ?>s">
                                 <?php $photo = get_post_thumbnail_id($id); ?>
-                                <?php if ($photo) { ?>
+                                <?php if ($photo): ?>
                                     <div class="photo">
-                                        <?php echo wp_get_attachment_image($photo, 'full'); ?>
+                                        <?php echo wp_get_attachment_image($photo, 'full');
+
+                                        $hover_text = get_field('hover_text', $id);
+                                        $text_single_page = get_field('text_single_page', $id);
+
+                                        if (!empty($hover_text) && !empty($text_single_page)) { ?>
+                                            <div class="hover-block">
+                                                <span><?php echo $hover_text; ?></span>
+                                            </div>
+                                        <?php } ?>
                                     </div>
-                                <?php } ?>
+                                <?php endif; ?>
                                 <h5><?php echo get_the_title($id); ?></h5>
-                                <?php $leadership_position = get_field('leadership_position', $id);
-                                if ($leadership_position) { ?>
+                                <?php
+                                $leadership_position = get_field('leadership_position', $id);
+                                if ($leadership_position): ?>
                                     <div class="position">
                                         <?php echo $leadership_position; ?>
                                     </div>
-                                <?php } ?>
+                                <?php endif; ?>
                             </a>
-                        <?php } ?>
+                            <?php $delay += 0.15; ?>
+                        <?php endforeach; ?>
                     </div>
-                <?php } ?>
+                <?php endif; ?>
             </div>
         </div>
     </section>
